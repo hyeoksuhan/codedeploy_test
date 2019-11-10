35e0e2ff4e6f484ba0a77de68f331d0026744b10
hshan <hyeoksu.han@gmail.com>
2019-11-10 21:03:55 +0900

    fix start script

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+home=/home/ec2-user
+appdir=$home/app
+
+source $home/.bash_profile
+
+cd $appdir
 
 npm i --production
 pm2 startOrRestart ecosystem.config.js --env production
